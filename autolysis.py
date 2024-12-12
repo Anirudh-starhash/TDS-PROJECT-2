e9f15f08b9265c34612746480d9d24b7c9886837
Anirudh Pabbaraju <anirudhpabbaraju1103@gmail.com>
2024-12-12 12:33:44 +0530

    added comments and imported required in autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -1,16 +1,17 @@
-# for connecting with llm and sending data requests and json 
+# for connecting with LLM and sending data -> requests and json 
 import requests 
 import json
 
-# for aiproxy token from environment variables os
+# for aiproxy token from environment variables -> os
 import os
 
-#pandas and numpy for manipulating the dataframe 
+# ->pandas and numpy for manipulating the dataframe 
 import pandas as pd
 import numpy as np
 
-# for data visualization
+# for data visualization -> seaborn
 import seaborn as sb
 
 AIPROXY_TOKEN = os.environ["AIPROXY_TOKEN"]
+
 
