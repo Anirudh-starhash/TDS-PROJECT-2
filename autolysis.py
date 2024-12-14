be8cbe3935fafc2555b64d0f76d2ba5d15c6efc7
Anirudh Pabbaraju <anirudhpabbaraju1103@gmail.com>
2024-12-14 20:48:02 +0530

    x

--- autolysis.py
+++ autolysis.py
@@ -1,3 +1,15 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "seaborn",
+#   "pandas",
+#   "matplotlib",
+#   "httpx",
+#   "chardet",
+#   "numpy",
+# ]
+# ///
+
 import requests
 import json
 import os
